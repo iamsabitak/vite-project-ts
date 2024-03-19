671f703b515b97ff42a8820165cf52f9ac6ba102
sabita <sabitakhadka091@gmail.com>
2024-03-19 10:27:32 +0545

    change the component name from counterApp to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Counter from "./Counter";
 
-const CounterApp: React.FC = () => {
+const App: React.FC = () => {
   return (
     <div>
       <Counter />
@@ -9,4 +9,4 @@ const CounterApp: React.FC = () => {
   );
 };
 
-export default CounterApp;
+export default App;
