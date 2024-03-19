4906725c59d173f719d3e076f9202e0c060eeaf3
sabita <sabitakhadka091@gmail.com>
2024-03-19 12:23:15 +0545

    Added Login form with state for email and password, utilizing useState, and integrated form handling into JSX.

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface IState {
+  email: string;
+  password: string;
+}
 
 const Login: React.FC = () => {
+  const [inputValue, setInputValue] = useState<IState>({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue({ ...inputValue, [event.target.name]: event.target.value });
+  };
+  const onHandleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log(inputValue);
+  };
+
   return (
     <>
       <h1>Login Form</h1>
+      <form onSubmit={onHandleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email Address"
+          value={inputValue.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          value={inputValue.password}
+          onChange={handleChange}
+        />
+        <button type="submit">submit</button>
+      </form>
     </>
   );
 };
