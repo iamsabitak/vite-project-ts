d269ee6bed31ac288662aaaba95f2e51f34e3219
sabita <sabitakhadka091@gmail.com>
2024-03-19 10:20:11 +0545

    created a increment and decrement function which increase and decrease the counter value

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,1 +1,24 @@
+import React, { useState } from "react";
 
+const Counter: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
