2db1eb65566630946b0aaa760346d0321ec45689
sabita <sabitakhadka091@gmail.com>
2024-03-19 10:40:30 +0545

    implemented the customer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Counter from "./Counter";
+import Customer from "./Customer";
 
 const App: React.FC = () => {
   return (
     <div>
       <Counter />
+      <Customer />
     </div>
   );
 };
