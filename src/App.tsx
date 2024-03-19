a14824abd90788f308a975341e3b060b84a2f1d8
sabita <sabitakhadka091@gmail.com>
2024-03-19 10:20:49 +0545

    added the counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import Counter from "./Counter";
 
 const CounterApp: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
-
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
-
   return (
     <div>
-      <h1>Counter App</h1>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter />
     </div>
   );
 };
