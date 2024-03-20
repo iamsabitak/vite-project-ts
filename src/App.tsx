ba12a11b05a0ba57e89f659230ea5f4510688c7c
sabita <sabitakhadka091@gmail.com>
2024-03-20 16:35:35 +0545

    implement the MemoizationExample in app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 // import Counter from "./Counter";
-import Customer from "./Customer";
+// import Customer from "./Customer";
+import Login from "./Login";
+import MemoizationExample from "./MemoizationExample";
 
 const App: React.FC = () => {
   return (
     <div>
       {/* <Counter /> */}
-      <Customer name="peter" email="sabitakhadka091@gmail.com" age={10} />
+      {/* <Customer name="peter" email="sabitakhadka091@gmail.com" age={10} /> */}
+      <Login />
+      <MemoizationExample />
     </div>
   );
 };
