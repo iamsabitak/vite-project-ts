1a8489fd9f402815d76630b10b4fc66b8e0cc25a
sabita <sabitakhadka091@gmail.com>
2024-03-21 16:40:02 +0545

    Fetch and display single user details

--- src/components/SingleUser.tsx
+++ src/components/SingleUser.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { IState } from "../context/UserContext";
 
 const SingleUser: React.FC = () => {
+  const [user, setUser] = useState<IState | null>();
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+      }
+      const data = await response.json();
+      console.log(data);
+      setUser(data);
+    };
+    fetchData();
+  }, [setUser, id]);
+  if (!user) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
       <h1>Single User</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <td scope="col">Name</td>
+            <td scope="col">UserName</td>
+            <td scope="col">Email</td>
+            <td scope="col">Address</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr key={user.id}>
+            <td scope="col">{user.username}</td>
+            <td scope="col">{user.name}</td>
+            <td scope="col">{user.email}</td>
+            <td scope="col">{user.address.city}</td>
+          </tr>
+        </tbody>
+        <Link to="/" className="btn btn-success">
+          Go Back
+        </Link>
+      </table>
     </>
   );
 };
