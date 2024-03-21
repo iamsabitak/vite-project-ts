ad9ebea60bb3fa6b6d5ad4ae9bf7a701d4bf8c2f
sabita <sabitakhadka091@gmail.com>
2024-03-21 12:37:33 +0545

    added the condition to throw the error the to check the response

--- src/components/FetchApi.tsx
+++ src/components/FetchApi.tsx
@@ -2,38 +2,23 @@ import React, { useEffect } from "react";
 import UserInformation from "./UserInformation";
 import useUserContext from "../context/useUserContext";
 
-export interface IState {
-  id: number;
-  name: string;
-  email: string;
-  username: string;
-  address: {
-    street: string;
-  };
-}
-
 const FetchApi: React.FC = () => {
   const { setUser } = useUserContext();
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setUser(data);
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
       }
+      const data = await response.json();
+      console.log(data);
+      setUser(data);
     };
     fetchData();
   }, [setUser]);
-
   return (
     <>
       <h1>FetchApi</h1>
@@ -41,5 +26,4 @@ const FetchApi: React.FC = () => {
     </>
   );
 };
-
 export default FetchApi;
