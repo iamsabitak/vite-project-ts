3dea79a8c4cece8f1367f947ac6932fa572680b3
sabita <sabitakhadka091@gmail.com>
2024-03-21 15:21:11 +0545

    added routes to add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from "react";
-// import Counter from "./components/Counter";
-// import Customer from "./components/Customer";
-// import Login from "./components/Login";
-// import MemoizationExample from "./components/MemoizationExample";
+
+import Counter from "./components/Counter";
+import Customer from "./components/Customer";
+import MemoizationExample from "./components/MemoizationExample";
+import Login from "./components/Login";
 import FetchApi from "./components/FetchApi";
+
 import { UserProvider } from "./context/UserContext";
+import { Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <UserProvider>
-      {/* <Counter /> */}
-      {/* <Customer name="peter" email="sabitakhadka091@gmail.com" age={10} /> */}
-      {/* <Login /> */}
-      {/* <MemoizationExample /> */}
-      <FetchApi />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<FetchApi />} />
+        <Route path="/" element={<Counter />} />
+        <Route
+          path="/"
+          element={
+            <Customer name="peter" email="sabitakhadka091@gmail.com" age={10} />
+          }
+        />
+        <Route path="/" element={<MemoizationExample />} />
+      </Routes>
     </UserProvider>
   );
 };
