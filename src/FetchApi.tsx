444a6af6268032f8eefd19f027339ce6e4404a31
sabita <sabitakhadka091@gmail.com>
2024-03-21 11:03:39 +0545

    get data of user and display it in a table

--- src/FetchApi.tsx
+++ src/FetchApi.tsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface IState {
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+  address: {
+    street: string;
+  };
+}
 
 const FetchApi: React.FC = () => {
-    return (
-        <h1>FetchApi</h1>
-    )
+  const [user, setUser] = useState<IState[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      console.log(data);
+      setUser(data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+      <h1>FetchApi</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>UserName</td>
+            <td>Email</td>
+            <td>Address</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          {user.map((user) => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.address.street}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 export default FetchApi;
