1ce179fa0ef48a7b4d3cf6e185e7f367ee55a1d9
sabita <sabitakhadka091@gmail.com>
2024-03-21 11:09:59 +0545

    comment all other import except fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Counter from "./components/Counter";
-import Customer from "./components/Customer";
+// import Counter from "./components/Counter";
+// import Customer from "./components/Customer";
 import Login from "./components/Login";
 import MemoizationExample from "./components/MemoizationExample";
 import FetchApi from "./components/FetchApi";
@@ -8,10 +8,10 @@ import FetchApi from "./components/FetchApi";
 const App: React.FC = () => {
   return (
     <div>
-      <Counter />
-      <Customer name="peter" email="sabitakhadka091@gmail.com" age={10} />
-      <Login />
-      <MemoizationExample />
+      {/* <Counter /> */}
+      {/* <Customer name="peter" email="sabitakhadka091@gmail.com" age={10} /> */}
+      {/* <Login /> */}
+      {/* <MemoizationExample /> */}
       <FetchApi/>
     </div>
   );
