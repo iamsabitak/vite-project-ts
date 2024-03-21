f8cc8e3f29d6fe6f664a298abc81ebbc12b4e4fd
sabita <sabitakhadka091@gmail.com>
2024-03-21 11:08:37 +0545

    create a new file and move all the component there

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-// import Counter from "./Counter";
-// import Customer from "./Customer";
-import Login from "./Login";
-import MemoizationExample from "./MemoizationExample";
-import FetchApi from "./FetchApi";
+import Counter from "./components/Counter";
+import Customer from "./components/Customer";
+import Login from "./components/Login";
+import MemoizationExample from "./components/MemoizationExample";
+import FetchApi from "./components/FetchApi";
 
 const App: React.FC = () => {
   return (
     <div>
-      {/* <Counter /> */}
-      {/* <Customer name="peter" email="sabitakhadka091@gmail.com" age={10} /> */}
+      <Counter />
+      <Customer name="peter" email="sabitakhadka091@gmail.com" age={10} />
       <Login />
       <MemoizationExample />
       <FetchApi/>
