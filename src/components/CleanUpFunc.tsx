454127a40240fcc9d5ca9311c51981946e3e8af0
sabita <sabitakhadka091@gmail.com>
2024-03-22 12:03:36 +0545

    Add cleanup function to remove event listener in CleanUp component

--- src/components/CleanUpFunc.tsx
+++ src/components/CleanUpFunc.tsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 const CleanUp: React.FC = () => {
   const [count, setCount] = useState<number>(0);
 
   const increment = () => {
     setCount((prevCount) => prevCount + 1);
   };
+  useEffect(() => {
+    const greet = () => {
+      console.log("Ni_hao");
+    };
+    document.addEventListener("click", greet);
+    return () => {
+    //   console.log("l1");
+      document.removeEventListener("click", greet);
+    };
+  }, [count]);
   return (
     <>
       <h1>Clean Up</h1>
