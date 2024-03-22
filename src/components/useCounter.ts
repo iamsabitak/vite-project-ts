0523b39e137b4dc3306f54b85031e2379c52e41b
sabita <sabitakhadka091@gmail.com>
2024-03-22 13:41:24 +0545

    create UseCustomReturnType

--- src/components/useCounter.ts
+++ src/components/useCounter.ts
@@ -1,16 +1,23 @@
 import { useState } from "react";
 
-function useCustom() {
+type UseCustomReturnType = {
+  count: number;
+  addOne: () => void;
+  minusOne: () => void;
+};
+
+function useCustom(): UseCustomReturnType {
   const [count, setCount] = useState<number>(0);
 
-  const addOne = () => {
-    setCount(count + 1);
-  };
-  const minusOne = () => {
-    setCount(count - 1);
+  const addOne = (): void => {
+    setCount((prevCount: number) => prevCount + 1);
   };
 
-  return [count, addOne, minusOne];
+  const minusOne = (): void => {
+    setCount((prevCount: number) => prevCount - 1);
+  };
+
+  return { count, addOne, minusOne };
 }
 
 export default useCustom;
