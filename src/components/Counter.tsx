3037aeac6bc1284896e12bd7aecc05b9b78d4fb8
sabita <sabitakhadka091@gmail.com>
2024-03-22 11:25:57 +0545

    implement the clean up funtion

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import CleanUp from "./CleanUpFunc";
 
 const Counter: React.FC = () => {
   const [count, setCount] = useState<number>(0);
@@ -17,6 +18,7 @@ const Counter: React.FC = () => {
       <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <CleanUp />
     </div>
   );
 };
