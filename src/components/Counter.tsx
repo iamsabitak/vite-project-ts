a3a9725306b46578dacb39dda6d6374b11e0596c
sabita <sabitakhadka091@gmail.com>
2024-03-22 13:41:58 +0545

    Implement Counter component with useCustom hook

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import CleanUp from "./CleanUpFunc";
+import useCustom from "./useCounter";
 
 const Counter: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
-
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
-
+  const { count, addOne, minusOne } = useCustom();
   return (
     <div>
       <h1>Counter App</h1>
       <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <button onClick={addOne}>Increment</button>
+      <button onClick={minusOne}>Decrement</button>
       <CleanUp />
     </div>
   );
