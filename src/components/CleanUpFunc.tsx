728cd669251afd8151e8eaddeb70ede9c58d74a1
sabita <sabitakhadka091@gmail.com>
2024-03-22 11:25:11 +0545

    added a counter that increase the counter

--- src/components/CleanUpFunc.tsx
+++ src/components/CleanUpFunc.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 const CleanUp: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
   return (
     <>
       <h1>Clean Up</h1>
+      <button onClick={increment}>Increment</button>
+      <span>Count: {count}</span>
     </>
   );
 };
