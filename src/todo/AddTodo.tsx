27127591f2dcd151e7bfa0ba81f302500cc2d3e0
sabita <sabitakhadka091@gmail.com>
2024-03-25 11:08:29 +0545

    renamed the component name

--- src/todo/AddTodo.tsx
+++ src/todo/AddTodo.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const AddTodo: React.FC = () => {
+const addTodo: React.FC = () => {
   return (
     <>
       <h1>AddTodo</h1>
     </>
   );
 };
-export default AddTodo;
+export default addTodo;
