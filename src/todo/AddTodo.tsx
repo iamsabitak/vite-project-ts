582bff46a1fe108d3c75bbb2127844c9033f6b9c
sabita <sabitakhadka091@gmail.com>
2024-03-25 11:45:15 +0545

    created the todo app which add and remobe the todo

--- src/todo/AddTodo.tsx
+++ src/todo/AddTodo.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface Todo {
+  todo: string;
+}
 
 const AddTodo: React.FC = () => {
+  const [name, setName] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const addTodoHandler = () => {
+    setTodos([...todos, { todo: name }]);
+    setName(''); 
+  };
+
+  const removeTodoHandler = (index: number) => {
+    setTodos(prevTodos => prevTodos.filter((_, itemIndex) => itemIndex !== index));
+  };
+
   return (
     <>
       <h1>AddTodo</h1>
+      <input
+        type="text"
+        name="name"
+        placeholder="Enter your Todo"
+        value={name}
+        onChange={onChangeHandler}
+      />
+      <button onClick={addTodoHandler}>Add</button>
+      <div>
+        {todos.map((item, index) => {
+          return (
+            <div key={index}>
+              <h1>{item.todo}</h1>
+              <button onClick={() => removeTodoHandler(index)}>Remove</button>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
