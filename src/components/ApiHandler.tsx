a71508ec22d70f644a503fff84a11bbba503766a
sabita <sabitakhadka091@gmail.com>
2024-03-26 16:53:20 +0545

    CRETAED A APIHANDLER FUNCTION WHICH GET THE API

--- src/components/ApiHandler.tsx
+++ src/components/ApiHandler.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  id: number;
+}
 
 const ApiHandler: React.FC = () => {
-  return <div>ApiHandler</div>;
+  const [user, setUser] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
+      setUser(data.users);
+    };
+    fetchApi();
+  }, []);
+
+  return (
+    <div>
+      ApiHandler
+      {user.map((data) => {
+        return <li>{data.firstName}</li>;
+      })}
+    </div>
+  );
 };
 
 export default ApiHandler;
