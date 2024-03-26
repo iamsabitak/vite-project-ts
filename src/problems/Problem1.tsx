f810a0b4429696e8d43f2afd3c20ab5dfdf31af9
sabita <sabitakhadka091@gmail.com>
2024-03-26 14:31:39 +0545

    Render country names as a list in Problem1 component

--- src/problems/Problem1.tsx
+++ src/problems/Problem1.tsx
@@ -1,6 +1,35 @@
 import React from "react";
+interface COUNTRIES {
+  name: string;
+  value: string;
+  cities: string[];
+}
 
 const Problem1: React.FC = () => {
-  return <h1>Problem</h1>;
+  const countries: COUNTRIES[] = [
+    {
+      name: "USA",
+      value: "US",
+      cities: ["New York", "Los Angeles", "Chicago"],
+    },
+    {
+      name: "Japan",
+      value: "TK",
+      cities: ["Tokyo", "Osaka", "Kyoto"],
+    },
+    {
+      name: "France",
+      value: "FR",
+      cities: ["Paris", "Marseille", "Lyon"],
+    },
+  ];
+  return (
+    <h1>
+      Problem{" "}
+      {countries.map((item) => {
+        return <li>{item.name}</li>;
+      })}
+    </h1>
+  );
 };
 export default Problem1;
