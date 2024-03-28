4106336d5fbad1aad665b2372d8efeddb588ce60
sabita <sabitakhadka091@gmail.com>
2024-03-28 15:55:15 +0545

    used mantine to design the addtodo and added two extra textinput

--- src/todo/AddTodo.tsx
+++ src/todo/AddTodo.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Popup from "../components/Popup";
+import { Box, Button, Text, TextInput, Title } from "@mantine/core";
 
 interface Todo {
   id: number;
@@ -30,28 +31,64 @@ const AddTodo: React.FC = () => {
   };
 
   return (
-    <>
-      <h1>AddTodo</h1>
-      <input
+    <Box>
+      <Title>AddTodo</Title>
+      <TextInput
         type="text"
         name="name"
         placeholder="Todo"
         value={name}
         onChange={onChangeHandler}
+        w={500}
       />
-      <button onClick={addTodoHandler}>Add</button>
-      <div>
+      <Button onClick={addTodoHandler}>Add</Button>
+      <Box>
         {todos.map((item, index) => {
           return (
             <div key={item.id}>
               <h1>{item.text}</h1>
-              <button onClick={() => removeTodoHandler(index)}>Remove</button>
+              <Button onClick={() => removeTodoHandler(index)}>Remove</Button>
             </div>
           );
         })}
         <Popup />
-      </div>
-    </>
+      </Box>
+
+      <Box>
+        <TextInput
+          w={300}
+          placeholder="Your name"
+          label={
+            <Text
+              sx={{
+                color: "orange",
+              }}
+            >
+              FullName:
+            </Text>
+          }
+          sx={{
+            input: {
+              backgroundColor: "#fcd0d0",
+              border: "none",
+            },
+          }}
+        />
+        <br />
+        <TextInput
+          w={300}
+          placeholder="disabled input"
+          disabled
+          sx={{
+            background: "#FAA0A0",
+            borderRadius: "6px",
+            input: {
+              border: "none",
+            },
+          }}
+        />
+      </Box>
+    </Box>
   );
 };
 
